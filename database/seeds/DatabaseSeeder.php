752868e60d0e17c0468ffb361133c3cc31a10617
Deep Panchal <deep@fes.org.in>
2023-09-13 13:06:22 +0530

    uploading first phase code

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name' => 'Deep Panchal',
+            'email' => 'deep@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $wings = array("A", "B", "C", "D", "E", "F", "G", "H");
+        foreach ($wings as $wing) {
+            DB::table('wing')->insert(['name' => $wing]);
+        }
+
+        $roles = array("Secretary", "Treasurer", "Member");
+        foreach ($roles as $role) {
+            DB::table('role')->insert(['name' => $role]);
+        }
+
+        $acts = array("Completed", "Pending", "Initiated");
+        foreach ($acts as $act) {
+            DB::table('act')->insert(['name' => $act]);
+        }
+    }
+}
